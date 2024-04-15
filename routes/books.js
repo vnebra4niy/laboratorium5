5de37edbd7de3b00356b7d3d9a8c6166ecaec08b
vnebra4niy <119805901+vnebra4niy@users.noreply.github.com>
2024-04-15 20:10:44 +0200

    3

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const booksController = require('../controllers/books')
 
-router.get('/list', booksController.getBookList)
+const books = [
+    { id: 1, title: 'Book 1', author: 'Author 1' },
+    { id: 2, title: 'Book 2', author: 'Author 2' },
+    { id: 3, title: 'Book 3', author: 'Author 3' }
+];
+
+router.get('/list', (req, res) => {
+    res.render('books', { books: books })
+})
+
+router.get('/add', (req, res) => {
+    res.render('add-book');
+})
+
+router.post('/add', (req, res) => {
+    res.redirect('/books/list')
+})
+
+router.delete('/delete/:id', (req, res) => {
+    res.redirect('/books/list')
+})
 
 module.exports = router
