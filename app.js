a6e9242b96d94e1c0461bd02f77af064c35b3482
vnebra4niy <119805901+vnebra4niy@users.noreply.github.com>
2024-04-15 19:17:49 +0200

    1

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.use((req, res, next) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
